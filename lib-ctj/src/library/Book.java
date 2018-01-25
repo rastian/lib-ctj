7e12fdf54947d38cb96043d9f42d4ed1741bc623
Christian <bronxchris96@gmail.com>
2018-01-25 12:30:51 -0500

    Make getters/setters public in Book.java

--- lib-ctj/src/library/Book.java
+++ lib-ctj/src/library/Book.java
@@ -19,32 +19,32 @@ public class Book {
 	public Book(String title, String author, String age, String isbn, String filename) 
 			throws IOException, FileNotFoundException {
 		this.title = title;
-		this.author = author;
+		this.setAuthor(author);
 		this.age = age;
 		this.isbn = isbn;
 
 		wordMap = new HashMap<>();
 		// Populate wordMap
 		Stream<String> s = Files.lines(Paths.get(filename))
-				.flatMap(line -> Stream.of(line.split("[ ,.!;?\r\n]")));
-		s.filter(w -> w.length() > 0)
-		.forEach(w -> wordMap.put(w, wordMap.containsKey(w) ? wordMap.get(w) + 1 : 1));
+				.flatMap(line -> Stream.of(line.split("[ ,.!;?\r\n]")))
+				.filter(w -> w.length() > 0);
+		s.forEach(w -> wordMap.put(w, wordMap.containsKey(w) ? wordMap.get(w) + 1 : 1));
 		s.close();
 	}
 	
-	void  setTitle(String title) { this.title = title; }
-	String getTitle() { return title; }
+	public void  setTitle(String title) { this.title = title; }
+	public String getTitle() { return title; }
 	
-	void setAuthor(String author) { this.author = author; }
-	String getAuthor() { return author; }
+	public void setAuthor(String author) { this.author = author; }
+	public String getAuthor() { return author; }
 	
-	void setAge(String age) { this.age = age; }
-	String getAge() { return age; }
+	public void setAge(String age) { this.age = age; }
+	public String getAge() { return age; }
 	
-	void setISBN(String isbn) { this.isbn = isbn; }
-	String getISBN() { return isbn; }
+	public void setISBN(String isbn) { this.isbn = isbn; }
+	public String getISBN() { return isbn; }
 	
-	int getUniqueWords() { return uniqueWords; }
+	public int getUniqueWords() { return uniqueWords; }
 	
-	int getTotalCount() { return totalCount; }
+	public int getTotalCount() { return totalCount; }
 }
