a0395b25d9a0799970278680f9325c72908bdb92
Jacob DeVoy <jdevoy294@gmail.com>
2018-02-09 01:47:49 -0500

    "Open" and "Save" Buttons open open and save windows, and "Add Book" Menu opens a window to add a book.

--- lib-ctj/src/ui/Main.java
+++ lib-ctj/src/ui/Main.java
@@ -1,6 +1,8 @@
 package ui;
+import java.io.File;
 import javafx.application.*;
 import javafx.stage.*;
+import library.Book;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
@@ -25,22 +27,92 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 
 	}
 	public void start(Stage primaryStage) throws Exception{
+		final FileChooser fileChooser = new FileChooser();
 		primaryStage.setTitle("libctj");
+		Stage stage = new Stage();
+		FileChooser openLD = new FileChooser();
+		openLD.setTitle("Select Library or Dictionary XML File to Open");
 		ToolBar toolbar = new ToolBar(newLib, open, save, delete, addB, delB, filter, merge, genD);
+	
 		
 		newLib.setOnAction(e -> System.out.println("Thanks Obama"));
-		open.setOnAction(e -> System.out.println("Opened"));
-		save.setOnAction(e -> System.out.println("Saved"));
-		delete.setOnAction(e -> System.out.println("Deleted"));
-		addB.setOnAction(e -> System.out.println("Added Book"));
+		open.setOnAction(
+	            new EventHandler<ActionEvent>() {
+	                @Override
+	                public void handle(final ActionEvent e) {
+	                    File file = fileChooser.showOpenDialog(stage);
+	                }
+	            });
+		save.setOnAction(
+				new EventHandler<ActionEvent>() {
+	                @Override
+	                public void handle(final ActionEvent e) {
+	                    File file = fileChooser.showSaveDialog(stage);
+	                }
+	            });
+		delete.setOnAction(e -> System.out.println("Deleted Library"));
+		addB.setOnAction(
+				new EventHandler<ActionEvent>() {
+	                @Override
+	                public void handle(final ActionEvent e) {
+	                	Stage stageAdd = new Stage();
+	                	stageAdd.setTitle("Add Book");
+	                	GridPane grid = new GridPane();
+	                	grid.setPadding(new Insets(10, 10, 10, 10));
+	                	grid.setVgap(8);
+	                	grid.setHgap(10);
+	                	
+	                	Button browse = new Button("Browse");
+	                	browse.setOnAction(a -> fileChooser.showOpenDialog(stage));
+	                	
+	                	Button submit = new Button("Submit");
+	                	submit.setOnAction(a-> System.out.println("Bok Added"));
+	                	
+	                	Label titleLab = new Label("Title: ");
+	                	GridPane.setConstraints(titleLab, 0, 0);
+	                	
+	                	TextField titleInput = new TextField();
+	                	GridPane.setConstraints(titleInput, 1, 0);
+	                	
+	                	Label authLab = new Label("Author: ");
+	                	GridPane.setConstraints(authLab, 0, 1);
+	                	
+	                	TextField authInput = new TextField();
+	                	GridPane.setConstraints(authInput, 1, 1);
+	                	
+	                	
+	                	Label ageLab = new Label("Age: ");
+	                	GridPane.setConstraints(ageLab, 0, 2);
+	                	
+	                	TextField ageInput = new TextField();
+	                	GridPane.setConstraints(ageInput, 1, 2);
+	                	
+	                	Label isbnLab = new Label("ISBN: ");
+	                	GridPane.setConstraints(isbnLab, 0, 3);
+	                	
+	                	TextField isbnInput = new TextField();
+	                	GridPane.setConstraints(isbnInput, 1, 3);
+	                	
+	                	Label fileLab = new Label("Text File: ");
+	                	GridPane.setConstraints(fileLab, 0, 4);
+	                	
+	                	GridPane.setConstraints(browse, 1, 4);
+	                	GridPane.setConstraints(submit, 1, 5);
+	                	
+	                	grid.getChildren().addAll(titleLab, titleInput, authLab, authInput, ageLab, ageInput, isbnLab, isbnInput, fileLab, browse, submit);
+	                	Scene scene = new Scene(grid, 500, 500);
+	                    stageAdd.setScene(scene);
+	                    stageAdd.show();
+	                }
+	            });
 		delB.setOnAction(e -> System.out.println("Books Deleted"));
 		filter.setOnAction(e -> System.out.println("Libraries Filtered"));
 		merge.setOnAction(e -> System.out.println("Libraries Merged"));
 		genD.setOnAction(e -> System.out.println("Generate Dictionary"));
-		BorderPane pane = new BorderPane();
-		pane.setTop(toolbar);
-		Scene scene = new Scene(pane, 1000, 500);
-		primaryStage.setScene(scene);
+		BorderPane mainPane = new BorderPane();
+		mainPane.setTop(toolbar);
+		Scene mainScene = new Scene(mainPane, 1000, 500);
+		primaryStage.setScene(mainScene);
 		primaryStage.show();
 	}
 	@Override
