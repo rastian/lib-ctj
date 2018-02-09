09fa793e13957e254e159bdaee6bc9031550c07b
Jacob DeVoy <jdevoy294@gmail.com>
2018-02-09 08:19:18 -0500

    "Filter Library" Button opens a window to filter library by text fields. Tabs for libraries added to main window. "New Library" Button adds a new tab to the window.

--- lib-ctj/src/ui/Main.java
+++ lib-ctj/src/ui/Main.java
@@ -5,12 +5,14 @@ import javafx.stage.*;
 import library.Book;
 import javafx.scene.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 import javafx.event.*;
 
 public class Main extends Application implements EventHandler<ActionEvent>{
-
+	
 	Button newLib = new Button("New Library");
 	Button open = new Button("Open");
 	Button save = new Button("Save");
@@ -27,15 +29,35 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 
 	}
 	public void start(Stage primaryStage) throws Exception{
-		final FileChooser fileChooser = new FileChooser();
 		primaryStage.setTitle("libctj");
-		Stage stage = new Stage();
-		FileChooser openLD = new FileChooser();
-		openLD.setTitle("Select Library or Dictionary XML File to Open");
 		ToolBar toolbar = new ToolBar(newLib, open, save, delete, addB, delB, filter, merge, genD);
-	
 		
-		newLib.setOnAction(e -> System.out.println("Thanks Obama"));
+		//Tabs
+				TabPane tabPane = new TabPane();
+				Tab tab = new Tab();
+				tab.setText("new lib");
+				tabPane.getTabs().add(tab);
+				
+		//Open/Save File Stuff
+		final FileChooser fileChooser = new FileChooser();
+		Stage stage = new Stage(); //For Open/Save Fie
+		FileChooser openLD = new FileChooser(); 
+		openLD.setTitle("Select Library or Dictionary XML File to Open");
+		
+		//Button Actions
+		newLib.setOnAction( 
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(final ActionEvent e) {
+						
+						Tab tab = new Tab();
+						tab.setText("new lib");
+						tabPane.getTabs().add(tab);
+						
+						
+					}
+				});
+		
 		open.setOnAction(
 	            new EventHandler<ActionEvent>() {
 	                @Override
@@ -66,7 +88,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 	                	browse.setOnAction(a -> fileChooser.showOpenDialog(stage));
 	                	
 	                	Button submit = new Button("Submit");
-	                	submit.setOnAction(a-> System.out.println("Bok Added"));
+	                	submit.setOnAction(a-> System.out.println("Book Added"));
 	                	
 	                	Label titleLab = new Label("Title: ");
 	                	GridPane.setConstraints(titleLab, 0, 0);
@@ -106,11 +128,92 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 	                }
 	            });
 		delB.setOnAction(e -> System.out.println("Books Deleted"));
-		filter.setOnAction(e -> System.out.println("Libraries Filtered"));
+		filter.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(final ActionEvent e) {
+						Stage stageFilter = new Stage();
+	                	stageFilter.setTitle("Filter Books");
+	                	GridPane grid = new GridPane();
+	                	grid.setPadding(new Insets(10, 10, 10, 10));
+	                	grid.setVgap(8);
+	                	grid.setHgap(10);
+	                	
+	                	Button submit = new Button("Filter");
+	                	submit.setOnAction(a-> System.out.println("Library Filtered"));
+	                	
+	                	Label titleLab = new Label("Title: ");
+	                	GridPane.setConstraints(titleLab, 0, 0);
+	                	TextField titleInput = new TextField();
+	                	GridPane.setConstraints(titleInput, 1, 0);
+	                	
+	                	Label authLab = new Label("Author: ");
+	                	GridPane.setConstraints(authLab, 0, 1);
+	                	TextField authInput = new TextField();
+	                	GridPane.setConstraints(authInput, 1, 1);
+	                	
+	                	
+	                	Label ageLab = new Label("Age: ");
+	                	GridPane.setConstraints(ageLab, 0, 2);
+	                	TextField ageInput = new TextField();
+	                	GridPane.setConstraints(ageInput, 1, 2);
+	                	
+	                	Label isbnLab = new Label("ISBN: ");
+	                	GridPane.setConstraints(isbnLab, 0, 3);
+	                	TextField isbnInput = new TextField();
+	                	GridPane.setConstraints(isbnInput, 1, 3);
+	                	
+	                	Label uniqueLab = new Label("Unique Words: ");
+	                	GridPane.setConstraints(uniqueLab, 0, 4);
+	                	
+	                	Label uniqueEqualLab = new Label("=");
+	                	GridPane.setConstraints(uniqueEqualLab, 1,4);
+	                	TextField uniqueEqualInput = new TextField();
+	                	GridPane.setConstraints(uniqueEqualInput, 1, 5);
+	                	
+	                	Label uniqueGreaterLab = new Label(">");
+	                	GridPane.setConstraints(uniqueGreaterLab, 1, 6);
+	                	TextField uniqueGreaterInput = new TextField();
+	                	GridPane.setConstraints(uniqueGreaterInput, 1, 7);
+	                	
+	                	Label uniqueLessLab = new Label("<");
+	                	GridPane.setConstraints(uniqueLessLab, 1, 8);
+	                	TextField uniqueLessInput = new TextField();
+	                	GridPane.setConstraints(uniqueLessInput, 1, 9);
+	                	
+	                	Label totalLab = new Label("Total Words: ");
+	                	GridPane.setConstraints(totalLab, 0, 10);
+	                	
+	                	Label totalEqualLab = new Label("=");
+	                	GridPane.setConstraints(totalEqualLab, 1,10);
+	                	TextField totalEqualInput = new TextField();
+	                	GridPane.setConstraints(totalEqualInput, 1, 11);
+	                	
+	                	Label totalGreaterLab = new Label(">");
+	                	GridPane.setConstraints(totalGreaterLab, 1, 12);
+	                	TextField totalGreaterInput = new TextField();
+	                	GridPane.setConstraints(totalGreaterInput, 1, 13);
+	                	
+	                	Label totalLessLab = new Label("<");
+	                	GridPane.setConstraints(totalLessLab, 1, 14);
+	                	TextField totalLessInput = new TextField();
+	                	GridPane.setConstraints(totalLessInput, 1, 15);
+	                	
+	                	GridPane.setConstraints(submit, 1, 16);
+	                	
+	                	grid.getChildren().addAll(titleLab, titleInput, authLab, authInput, ageLab, ageInput, isbnLab, isbnInput, uniqueLab, uniqueEqualLab, uniqueEqualInput, uniqueGreaterLab, uniqueGreaterInput, uniqueLessLab, uniqueLessInput, totalLab, totalEqualLab, totalEqualInput, totalGreaterLab, totalGreaterInput, totalLessLab, totalLessInput, submit);
+	                	Scene scene = new Scene(grid, 1000, 600);
+	                    stageFilter.setScene(scene);
+	                    stageFilter.show();
+					}
+				});;
 		merge.setOnAction(e -> System.out.println("Libraries Merged"));
 		genD.setOnAction(e -> System.out.println("Generate Dictionary"));
+		
+		//Main Panes/Show Stage
 		BorderPane mainPane = new BorderPane();
 		mainPane.setTop(toolbar);
+		mainPane.setCenter(tabPane);
 		Scene mainScene = new Scene(mainPane, 1000, 500);
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
