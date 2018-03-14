ae55e2e61863e72ee923fb3aa769c253cb9279f4
Christian LaVerde <bronxchris96@gmail.com>
2018-03-14 00:12:28 -0400

    Create Dictionary constructor and DictElem class

---
+++ lib-ctj/src/main/library/DictElement.java
@@ -0,0 +1,49 @@
+package main.library;
+
+public class DictElement {
+	private boolean function;
+	private int frequency;
+	private int syllables;
+	private String spelling;
+	private String arpabet;
+	private String morphemes;
+	private String cognate;
+	private double biphAve;
+	private double pSegAve;
+	private String[] neighborhood;
+	
+	public DictElement() {
+		neighborhood = null;
+	}
+	
+
+	public boolean isFunction() { return function; }
+	public void setFunction(boolean function) { this.function = function; }
+
+	public int getFrequency() { return frequency; }
+	public void setFrequency(int frequency) { this.frequency = frequency; }
+
+	public int getSyllables() { return syllables; }
+	public void setSyllables(int syllables) { this.syllables = syllables; }
+
+	public String getSpelling() { return spelling; }
+	public void setSpelling(String spelling) { this.spelling = spelling; }
+
+	public String getArpabet() { return arpabet; }
+	public void setArpabet(String arpabet) { this.arpabet = arpabet; }
+
+	public String getMorphemes() { return morphemes; }
+	public void setMorphemes(String morphemes) { this.morphemes = morphemes; }
+
+	public String getCognate() { return cognate; }
+	public void setCognate(String cognate) { this.cognate = cognate; }
+
+	public double getBiphAve() {return biphAve; }
+	public void setBiphAve(double biphAve) {this.biphAve = biphAve; }
+
+	public double getpSegAve() { return pSegAve; }
+	public void setpSegAve(double pSegAve) { this.pSegAve = pSegAve; }
+
+	public String[] getNeighborhood() { if (neighborhood != null) return neighborhood; else return null; }
+	public void setNeighborhood(String[] neighborhood) { this.neighborhood = neighborhood; }
+}
--- lib-ctj/src/main/library/Dictionary.java
+++ lib-ctj/src/main/library/Dictionary.java
@@ -1,5 +1,112 @@
 package main.library;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class Dictionary {
+	private Path path;
+	private ArrayList<DictElement> entries;
+	private int size;
+	private Document doc;
+	private Element root;
 	
+	public Dictionary(Path dictXMLPath) {
+		path = dictXMLPath;
+		entries = new ArrayList<>();
+		File dictXMLFile = new File(path.toString());
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		
+		try {
+			// Read dictionary from XML file into memory
+			dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(dictXMLFile);
+			doc.getDocumentElement().normalize();
+			
+			root = doc.getDocumentElement();
+			
+			// Get all entries
+			NodeList entryNodes = doc.getElementsByTagName("W");
+			DictElement tmpEntry;
+			this.size = 0;
+			for (int i = 0; i < entryNodes.getLength(); ++i) {
+				Node entryNode = entryNodes.item(size);
+				if (entryNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element entry = (Element) entryNode;
+					tmpEntry = new DictElement();
+					
+					// Fill attributes
+					tmpEntry.setFunction(entry.getAttribute("function").trim().equals("yes"));
+					tmpEntry.setFrequency(Integer.parseInt(entry.getAttribute("frequency").trim()));
+					tmpEntry.setSyllables(Integer.parseInt(entry.getAttribute("syllables").trim()));
+					tmpEntry.setSpelling(entry.getAttribute("spelling").trim());
+					
+					NodeList childNodes = entry.getChildNodes();
+					for (int j = 0; j < childNodes.getLength(); ++j) {
+						Node childNode = childNodes.item(j);
+						if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element eNode = (Element) childNode;
+							switch (eNode.getTagName()) {
+							case "Arpabet": 
+								tmpEntry.setArpabet(eNode.getTextContent().trim());
+								break;
+							case "Morphemes":
+								tmpEntry.setMorphemes(eNode.getTextContent().trim());
+								break;
+							case "Cognate":
+								tmpEntry.setCognate(eNode.getTextContent().trim());
+								break;
+							case "BiphAve":
+								tmpEntry.setBiphAve(Double.parseDouble(eNode.getTextContent().trim()));
+								break;
+							case "PSegAve":
+								tmpEntry.setpSegAve(Double.parseDouble(eNode.getTextContent().trim()));
+								break;
+							case "Neighborhood":
+								// do thing
+								//System.out.println(eNode.getTextContent().trim());
+								NodeList neighborList = eNode.getElementsByTagName("Neighbor");
+								String[] neighborhood = new String[neighborList.getLength()];
+								for (int k = 0; k < neighborList.getLength(); ++k) {
+									Node neighborNode = neighborList.item(k);
+									if (neighborNode.getNodeType() == Node.ELEMENT_NODE) {
+										Element neighborElem = (Element) neighborNode;
+										neighborhood[k] = neighborElem.getTextContent();
+									}
+								}
+								tmpEntry.setNeighborhood(neighborhood);
+								break;
+							default:
+								break;
+							}
+						}
+					}
+					++size;
+					entries.add(tmpEntry);
+				}
+			}
+		}
+		catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		catch (SAXException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
